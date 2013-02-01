15ca8ea3a8841457fa6cd16324db218caec5fa74
Justin Molineaux <justin.molineaux@gmail.com>
2013-01-31 22:39:16 -0500

    Namespacing Notifications::Processor correctly.

--- lib/notifications/processor.rb
+++ lib/notifications/processor.rb
@@ -1,13 +1,17 @@
-class Notifications::Processor
+module Notifications
 
-  def process( event )
-    followers = event.poster.followers
-    
-    users_to_notify = followers.select do |follower|
-      follower.wants_to_hear_about? event
+  class Processor
+
+    def process( event )
+      followers = event.poster.followers
+      
+      users_to_notify = followers.select do |follower|
+        follower.wants_to_hear_about? event
+      end
+
+      CommitNotificationMailer.commit_notification( event, users_to_notify ).deliver
     end
 
-    CommitNotificationMailer.commit_notification( event, users_to_notify ).deliver
   end
 
 end
