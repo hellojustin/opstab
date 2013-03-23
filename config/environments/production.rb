5ea8ab1756302b3f8ade09958c3c5b66c0e89513
Justin Molineaux <justin.molineaux@gmail.com>
2013-03-22 23:31:31 -0400

    Adding ga.js to precompile list.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ CommitNotifier::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  
+  config.assets.precompile += 'ga.js'
 
   # Generate digests for assets URLs
   config.assets.digest = true
